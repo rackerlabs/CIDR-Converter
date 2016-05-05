e3658a0287d6d6a8f3e9a638e0a96dfcd0e9a393
davismartin <davisshippmartin@gmail.com>
2016-05-05 13:40:41 -0700

    chore(router): router not needed

--- app/config/routes.js
+++
@@ -1,9 +0,0 @@
-var React = require('react');
-var Main = require('../components/Main');
-var Router = require('react-router');
-var Route = Router.Route;
-
-module.exports = (
-  <Route path="/" component={Main}>
-  </Route>
-);
